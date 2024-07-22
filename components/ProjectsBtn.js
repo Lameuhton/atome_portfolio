b46e77f74abe3b0c4a2bd5a9256f2a347cd6d843
Lameuhton <aurelianeg2000@gmail.com>
2024-07-22 14:27:38 +0200

    Refactor ProjectsBtn component for improved performance and responsiveness

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -11,19 +11,18 @@ const ProjectsBtn = () => {
   return (
     <div className="mx-auto xl:mx-0">
       <Link
-        href={'/work'}
-        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+        href={"/work"}
+        className="relative w-[140px] h-[140px] sm:w-[185px] sm:h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
       >
         <Image
-          src={'/rounded-text.png'}
+          src={"/rounded-text.png"}
           width={141}
           height={148}
-          alt=''
-          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[100px] max-h-[107px] sm:max-w-[141px] sm:max-h-[148px]"
         />
-        <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300' />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
       </Link>
-
     </div>
   );
 };
