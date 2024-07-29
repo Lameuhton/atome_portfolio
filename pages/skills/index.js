7a297a127d005282d8b04ec6199a1a9487e07f52
Lameuhton <aurelianeg2000@gmail.com>
2024-07-29 09:49:24 +0200

    finished tech skills

--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,4 +1,3 @@
-
 // components
 import ServiceSlider from "../../components/ServiceSlider";
 import Bulb from "../../components/Bulb";
@@ -17,18 +16,36 @@ const Skills = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8 cursor-default">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8 cursor-default"
+            >
               Tech skills <span className="text-accent">.</span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0 cursor-default">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0 cursor-default"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
+            </motion.p>
           </div>
-          <div className="w-full xl:max-w-[65%]">
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
             {/* slider */}
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       <Bulb />
