375ae20f0517cd779e0d3356879dd7c2d7542b8d
Lameuhton <aurelianeg2000@gmail.com>
2024-08-23 09:46:40 +0200

    refactor: Update WorkSlider component with new work data and remove unused code

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,5 @@
-// service data
-const serviceData = [
+// work data
+const workData = [
   {
     title: "Immo API",
     description:
@@ -44,22 +44,9 @@ const serviceData = [
   },
 ];
 
-// // import swiper react components
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// // import swiper styles
-// import "swiper/css";
-// import "swiper/css/free-mode";
-// import "swiper/css/pagination";
-
-// // import required modules
-// import { Pagination } from "swiper";
-
-// icons
+// icons icons
 import { BsArrowRight } from "react-icons/bs";
 
-// // next image
-// import Image from "next/image";
 // import swiper react components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -67,19 +54,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-
-// icons
-import {
-  FaPython,
-  FaJava,
-  FaPhp,
-  FaCopyright,
-  FaHtml5,
-  FaCss3Alt,
-  FaSass,
-  FaGit,
-} from "react-icons/fa";
-import { TbSql } from "react-icons/tb";
 
 // import required modules
 import { FreeMode, Pagination } from "swiper";
@@ -108,7 +82,7 @@ const WorkSlider = () => {
       modules={[FreeMode, Pagination]}
       className="h-[250px] sm:h-[310px] xl:h-[330px]"
     >
-      {serviceData.map((item, index) => {
+      {workData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
             <div className="relative bg-[rgba(65,47,123,0.15)] h-max rounded-lg overflow-hidden p-6 flex flex-col items-start gap-x-6 sm:gap-x-0 group">
@@ -152,61 +126,6 @@ const WorkSlider = () => {
       })}
     </Swiper>
   );
-  // return (
-  //   <Swiper
-  //     spaceBetween={10}
-  //     pagination={{
-  //       clickable: true,
-  //     }}
-  //     modules={[Pagination]}
-  //     className="h-[280px] sm:h-[480px]"
-  //   >
-  //     {workSlides.slides.map((slide, index) => {
-  //       return (
-  //         <SwiperSlide key={index}>
-  //           <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
-  //             {slide.images.map((image, index) => {
-  //               return (
-  //                 <div
-  //                   className="relative rounded-lg overflow-hidden flex items-center justify-center group"
-  //                   key={index}
-  //                 >
-  //                   <div className="flex items-center justify-center relative overflow-hidden group">
-  //                     {/* image */}
-  //                     <Image
-  //                       src={image.path}
-  //                       width={500}
-  //                       height={300}
-  //                       alt={image.title}
-  //                       className="transition-transform duration-700 transform group-hover:scale-105"
-  //                     />
-  //                     {/* overlay gradient */}
-  //                     <div className="absolute inset-0 bg-gradient-to-r from-[#FF6347] via-[#FF4500] to-[#FF0000] opacity-0 group-hover:opacity-80 transition-opacity duration-700"></div>
-  //                     {/* title */}
-  //                     <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-  //                       <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-  //                         {/* title part 1 */}
-  //                         <div className="delay-100">SEE</div>
-  //                         {/* title part 2 */}
-  //                         <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-  //                           THE CODE
-  //                         </div>
-  //                         {/* icon */}
-  //                         <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-  //                           <BsArrowRight />
-  //                         </div>
-  //                       </div>
-  //                     </div>
-  //                   </div>
-  //                 </div>
-  //               );
-  //             })}
-  //           </div>
-  //         </SwiperSlide>
-  //       );
-  //     })}
-  //   </Swiper>
-  // );
 };
 
 export default WorkSlider;
